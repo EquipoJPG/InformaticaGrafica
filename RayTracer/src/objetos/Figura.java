bfc90154efe9a9dcc3a27782651346712ee7d70b
Jaime <jai_mi_to@hotmail.es>
2015-11-19 13:16:15 +0100

    Updated STUFF (acabo de enterarme de para qué sirve el TODO en eclipse)

--- RayTracer/src/objetos/Figura.java
+++ RayTracer/src/objetos/Figura.java
@@ -1,5 +1,20 @@
 package objetos;
 
+import data.Rayo;
+import data.Vector4;
+
 public class Figura extends Objeto {
+
+	@Override
+	public Double interseccion(Rayo ray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector4 normal(Vector4 interseccion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- RayTracer/src/objetos/Triangulo.java
+++ RayTracer/src/objetos/Triangulo.java
@@ -1,5 +1,37 @@
 package objetos;
 
+import data.Rayo;
+import data.Vector4;
+
 public class Triangulo extends Objeto {
+
+	/* Atributos privados */
+	// TODO generate getters and setters
+	private Vector4 p1;
+	private Vector4 p2;
+	private Vector4 p3;
+	
+	/**
+	 * @param centro: centro de la esfera (0,0,0) seria lo correcto
+	 * @param radio: radio de la esfera
+	 */
+	public Triangulo(Vector4 p1, Vector4 p2, Vector4 p3, Material m){
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+		super.material = m;
+	}
+	
+	@Override
+	public Double interseccion(Rayo ray) {
+		
+		return null;
+	}
+
+	@Override
+	public Vector4 normal(Vector4 interseccion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
