8d99f1d5c081e4a75b4142dd4cacd9626dc295ee
Jaime <jai_mi_to@hotmail.es>
2015-11-19 16:03:34 +0100

    El triangulito está teóricamente done

--- RayTracer/src/objetos/Triangulo.java
+++ RayTracer/src/objetos/Triangulo.java
@@ -6,44 +6,91 @@ import data.Vector4;
 public class Triangulo extends Objeto {
 
 	/* Atributos privados */
-	// TODO generate getters and setters
 	private Vector4 p1;
 	private Vector4 p2;
 	private Vector4 p3;
-	
+
 	/**
-	 * @param centro: centro de la esfera (0,0,0) seria lo correcto
-	 * @param radio: radio de la esfera
+	 * @param centro:
+	 *            centro de la esfera (0,0,0) seria lo correcto
+	 * @param radio:
+	 *            radio de la esfera
 	 */
-	public Triangulo(Vector4 p1, Vector4 p2, Vector4 p3, Material m){
+	public Triangulo(Vector4 p1, Vector4 p2, Vector4 p3, Material m) {
 		this.p1 = p1;
 		this.p2 = p2;
 		this.p3 = p3;
 		super.material = m;
 	}
-	
+
 	@Override
 	public Double interseccion(Rayo ray) {
 		Vector4 a = ray.getOrigen();
 		Vector4 d = ray.getDireccion().normalise();
-		Vector4 term1 = Vector4.sub(p2, p1);
-		Vector4 term2 = Vector4.sub(p3, p1);
-		Vector4 n = Vector4.cross(term1, term2);
-		
+		Vector4 n = normal(null);
+
 		double denominador = Vector4.dot(d, n);
-		if(denominador == 0){
+
+		/* Comprobacion de si intersecciona */
+		if (denominador == 0) {
+			/* No intersecciona o no se ve */
 			return null;
-		}
-		else{
-			double numerador = Vector4.dot(Vector4.sub(p1,a),n);
-			return numerador / denominador;
+		} else {
+			/* Si que intersecciona */
+			double numerador = Vector4.dot(Vector4.sub(p1, a), n);
+			double landa = numerador / denominador;
+			/* Interseccion */
+			Vector4 p = Rayo.getInterseccion(ray, landa);
+
+			/*
+			 * Comprobacion de que el punto de interseccion cae dentro del
+			 * triangulo
+			 */
+			double s1 = Vector4.dot(Vector4.cross(Vector4.sub(p2, p1), Vector4.sub(p, p1)), n);
+			double s2 = Vector4.dot(Vector4.cross(Vector4.sub(p3, p2), Vector4.sub(p, p2)), n);
+			double s3 = Vector4.dot(Vector4.cross(Vector4.sub(p1, p3), Vector4.sub(p, p3)), n);
+
+			/* Comprobar que las tres 's' tienen el mismo signo */
+			if (Math.signum(s1) == Math.signum(s2) && Math.signum(s1) == Math.signum(s3)
+					&& Math.signum(s3) == Math.signum(s2)) {
+				return landa;
+			} else{
+				/* No hay interseccion dentro del triángulo */
+				return null;
+			}
+
 		}
 	}
 
 	@Override
 	public Vector4 normal(Vector4 interseccion) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector4 term1 = Vector4.sub(p2, p1);
+		Vector4 term2 = Vector4.sub(p3, p1);
+		return Vector4.cross(term1, term2);
+	}
+
+	public Vector4 getP1() {
+		return p1;
+	}
+
+	public void setP1(Vector4 p1) {
+		this.p1 = p1;
+	}
+
+	public Vector4 getP2() {
+		return p2;
+	}
+
+	public void setP2(Vector4 p2) {
+		this.p2 = p2;
+	}
+
+	public Vector4 getP3() {
+		return p3;
+	}
+
+	public void setP3(Vector4 p3) {
+		this.p3 = p3;
 	}
 
 }
