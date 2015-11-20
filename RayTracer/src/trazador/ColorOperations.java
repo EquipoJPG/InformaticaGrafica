9658e7a5ded3583fefa2c841cb0574f4d14b2b66
Patricia Lazaro <plazaro1994@gmail.com>
2015-11-20 12:02:59 +0100

    add

--- RayTracer/src/trazador/ColorOperations.java
+++ RayTracer/src/trazador/ColorOperations.java
@@ -5,9 +5,9 @@ import java.awt.Color;
 public class ColorOperations {
 	
 	public static Color add (Color c1, Color c2){
-		int red = (int) (Math.min(255, (c1.getRed() + c2.getRed())));
-		int green = (int) (Math.min(255, (c1.getGreen() + c2.getGreen())));
-		int blue = (int) (Math.min(255, (c1.getBlue() + c2.getBlue())));
+		int red = (int) (Math.min(255, (c1.getRed() + c2.getRed())/2));
+		int green = (int) (Math.min(255, (c1.getGreen() + c2.getGreen())/2));
+		int blue = (int) (Math.min(255, (c1.getBlue() + c2.getBlue())/2));
 		
 		return new Color(red, green, blue);
 	}
