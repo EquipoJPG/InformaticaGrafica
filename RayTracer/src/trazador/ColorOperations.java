15786bbbff7b3578b329d54f986a7c3370fb13d7
Patricia Lazaro <plazaro1994@gmail.com>
2015-11-20 11:53:43 +0100

    colorcillos

---
+++ RayTracer/src/trazador/ColorOperations.java
@@ -0,0 +1,23 @@
+package trazador;
+
+import java.awt.Color;
+
+public class ColorOperations {
+	
+	public static Color add (Color c1, Color c2){
+		int red = (int) (Math.min(255, (c1.getRed() + c2.getRed())));
+		int green = (int) (Math.min(255, (c1.getGreen() + c2.getGreen())));
+		int blue = (int) (Math.min(255, (c1.getBlue() + c2.getBlue())));
+		
+		return new Color(red, green, blue);
+	}
+	
+	public static Color escalar (Color c, double k){
+		int red = (int) (Math.min(255, (c.getRed() * k)));
+		int green = (int) (Math.min(255, (c.getGreen() * k)));
+		int blue = (int) (Math.min(255, (c.getBlue() * k)));
+		
+		return new Color(red, green, blue);
+	}
+
+}
