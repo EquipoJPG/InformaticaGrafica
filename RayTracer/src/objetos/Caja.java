d59f0ba1dc628edf6e977c691beb55b6d7e661c6
Jaime <jai_mi_to@hotmail.es>
2015-11-23 19:03:36 +0100

    ¿Cansado de tanto commit? ;3 Im glad to hear that

--- RayTracer/src/objetos/Caja.java
+++ RayTracer/src/objetos/Caja.java
@@ -3,12 +3,55 @@ package objetos;
 import data.Rayo;
 import data.Vector4;
 
-public class Caja extends Objeto{
+public class Caja extends Objeto {
+
+	// Atributos privados
+	Vector4 lowerBound;
+	Vector4 upperBound;
+	Figura conjuntoObjetos;
+
+	public Caja(Figura f) {
+		lowerBound = f.getLowerBound();
+		upperBound = f.getUpperBound();
+		conjuntoObjetos = f;
+	}
 
 	@Override
 	public Double interseccion(Rayo ray) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector4 p = ray.getOrigen();
+		Vector4 m = ray.getDireccion();
+		boolean h = false;
+		// Si cumple cualquiera de las 6 condiciones no intersecta
+		// Condicion 1
+		if (p.getX() < lowerBound.getX() && m.getX() <= 0) {
+			h = true;
+		}
+		// Condicion 2
+		if (p.getY() < lowerBound.getY() && m.getY() <= 0) {
+			h = true;
+		}
+		// Condicion 3
+		if (p.getZ() < lowerBound.getZ() && m.getZ() <= 0) {
+			h = true;
+		}
+		// Condicion 4
+		if (p.getX() > upperBound.getX() && m.getX() >= 0) {
+			h = true;
+		}
+		// Condicion 5
+		if (p.getY() > upperBound.getY() && m.getY() >= 0) {
+			h = true;
+		}
+		// Condicion 6
+		if (p.getZ() < upperBound.getZ() && m.getZ() >= 0) {
+			h = true;
+		}
+		if(h){
+			return null;
+		}
+		else{
+			return conjuntoObjetos.interseccion(ray);
+		}
 	}
 
 	@Override
@@ -25,14 +68,12 @@ public class Caja extends Objeto{
 
 	@Override
 	public Vector4 getLowerBound() {
-		// TODO Auto-generated method stub
-		return null;
+		return lowerBound;
 	}
 
 	@Override
 	public Vector4 getUpperBound() {
-		// TODO Auto-generated method stub
-		return null;
+		return upperBound;
 	}
 
 }
