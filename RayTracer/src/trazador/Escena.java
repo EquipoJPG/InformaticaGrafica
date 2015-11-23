5e6a731244aa36760ebaa0b9c985069f1afaa69d
Alejandro <donrondon40@gmail.com>
2015-11-23 20:00:49 +0100

    Nueva escena creada

--- RayTracer/src/trazador/Escena.java
+++ RayTracer/src/trazador/Escena.java
@@ -18,44 +18,59 @@ public class Escena {
 		/* Define los objetos de la escena */
 
 		// esferas
-		Esfera esfera1 = new Esfera(3, new Material(Color.RED, 0.9, 0.4, 50));
+//		Esfera esfera1 = new Esfera(3, new Material(Color.RED, 0.9, 0.4, 50));
 		Material mesf2 = new Material(Color.CYAN, 0.9, 0, 1);
-		Esfera esfera2 = new Esfera(new Vector4(20, -20, 20, 1), 5, mesf2);
+		Esfera esfera2 = new Esfera(new Vector4(20, 20, 20, 1), 5, mesf2);
 		
-		Material luz = new Material(Color.ORANGE, 0.9, 1, 1);
-		Esfera sol = new Esfera(new Vector4(40, -40, 40, 0), 5, luz);
+//		Material luz = new Material(Color.ORANGE, 0.9, 1, 1);
+//		Esfera sol = new Esfera(new Vector4(40, -40, 40, 0), 5, luz);
 
-		// planos
-		Vector4 p1Plano1 = new Vector4(0, -40, 30, 1);
-		Vector4 p2Plano1 = new Vector4(0, 40, 30, 1);
-		Vector4 p3Plano1 = new Vector4(0, -40, -30, 1);
-		Vector4 p4Plano1 = new Vector4(0, 40, -30, 1);
+		// PLANOS
+		
+		// pared izquierda (y = 0)
+		Vector4 p1Plano2 = new Vector4(50, 0, 50, 1);
+		Vector4 p2Plano2 = new Vector4(0, 0, 50, 1);
+		Vector4 p3Plano2 = new Vector4(50, 0, 0, 1);
+		Vector4 p4Plano2 = new Vector4(0, 0, 0, 1);
+		Plano plano2 = new Plano(p1Plano2, p2Plano2,
+				p3Plano2, p4Plano2, new Material(Color.GREEN, 1, 0, 0));
+		
+		// pared derecha (x = 0)
+		Vector4 p1Plano3 = new Vector4(0, 0, 50, 1);
+		Vector4 p2Plano3 = new Vector4(0, 50, 50, 1);
+		Vector4 p3Plano3 = new Vector4(0, 0, 0, 1);
+		Vector4 p4Plano3 = new Vector4(0, 50, 0, 1);
+		Plano plano3 = new Plano(p1Plano3, p2Plano3,
+				p3Plano3, p4Plano3, new Material(Color.BLUE, 1, 0, 0));
+		
+		// suelo (z = 0)
+		Vector4 p1Plano1 = new Vector4(0, 0, 0, 1);
+		Vector4 p2Plano1 = new Vector4(0, 50, 0, 1);
+		Vector4 p3Plano1 = new Vector4(50, 0, 0, 1);
+		Vector4 p4Plano1 = new Vector4(50, 50, 0, 1);
 		Plano plano1 = new Plano(p1Plano1, p2Plano1,
-				p3Plano1, p4Plano1, new Material(Color.GREEN, 0.5, 0.5, 10));
+				p3Plano1, p4Plano1, new Material(Color.RED, 1, 0, 0));
 		
 		// triangulos
-		Vector4 p1Triang1 = new Vector4(0, 0, 0, 1);
-		Vector4 p2Triang1 = new Vector4(-100, 100, 0, 1);
-		Vector4 p3Triang1 = new Vector4(30, 30, 0, 1);
-		Triangulo triangulo1 = new Triangulo(p1Plano1, p2Plano1, p3Plano1, new Material(Color.WHITE, 0.8, 0.2, 1));
+//		Vector4 p1Triang1 = new Vector4(0, 0, 0, 1);
+//		Vector4 p2Triang1 = new Vector4(-100, 100, 0, 1);
+//		Vector4 p3Triang1 = new Vector4(30, 30, 0, 1);
+//		Triangulo triangulo1 = new Triangulo(p1Plano1, p2Plano1, p3Plano1, new Material(Color.WHITE, 0.8, 0.2, 1));
 
-		Esfera esferaT1 = new Esfera(p1Plano1, 5, new Material(Color.RED, 0.2, 0.2, 1));
-		Esfera esferaT2 = new Esfera(p2Plano1, 5, new Material(Color.GREEN, 0.2, 0.2, 1));
-		Esfera esferaT3 = new Esfera(p3Plano1, 5, new Material(Color.BLUE, 0.2, 0.2, 1));
-		Esfera esferaT4 = new Esfera(p4Plano1, 5, new Material(Color.WHITE, 0.2, 0.2, 1));
+//		Esfera esferaT1 = new Esfera(p1Plano1, 5, new Material(Color.RED, 0.2, 0.2, 1));
+//		Esfera esferaT2 = new Esfera(p2Plano1, 5, new Material(Color.GREEN, 0.2, 0.2, 1));
+//		Esfera esferaT3 = new Esfera(p3Plano1, 5, new Material(Color.BLUE, 0.2, 0.2, 1));
+//		Esfera esferaT4 = new Esfera(p4Plano1, 5, new Material(Color.WHITE, 0.2, 0.2, 1));
 		
 		
 		// agrega todos los objetos
 //		objetos.add(esfera1);
 		objetos.add(esfera2);
 //		objetos.add(sol);
-		objetos.add(plano1);
-//		objetos.add(triangulo1);
 		
-//		objetos.add(esferaT1);
-//		objetos.add(esferaT2);
-//		objetos.add(esferaT3);
-//		objetos.add(esferaT4);
+		objetos.add(plano1);
+		objetos.add(plano2);
+		objetos.add(plano3);
 		
 		return objetos;
 	}
