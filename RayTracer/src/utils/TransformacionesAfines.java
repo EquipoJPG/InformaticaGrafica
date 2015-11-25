c1dd17a411f4093709196d06ce005d7a986f011d
Jaime <jai_mi_to@hotmail.es>
2015-11-25 19:23:27 +0100

    Added some cosas to transf afines

--- RayTracer/src/utils/TransformacionesAfines.java
+++ RayTracer/src/utils/TransformacionesAfines.java
@@ -1,5 +1,32 @@
 package utils;
 
+import Jama.Matrix;
+import Jama.LUDecomposition;
+
 public class TransformacionesAfines {
 
+	public static Matrix getIdentity() {
+		double[][] values = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 } };
+		return new Matrix(values);
+	}
+	
+	public static Matrix getXTraslation(double x){
+		double[][] values = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { x, 0, 0, 1 } };
+		return new Matrix(values);
+	}
+	
+	public static Matrix getYTraslation(double y){
+		double[][] values = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, y, 0, 1 } };
+		return new Matrix(values);
+	}
+	
+	public static Matrix getZTraslation(double z){
+		double[][] values = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, z, 1 } };
+		return new Matrix(values);
+	}
+	
+	public static Matrix getGeneralTraslation(double x, double y, double z){
+		double[][] values = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { x, y, z, 1 } };
+		return new Matrix(values);
+	}
 }
