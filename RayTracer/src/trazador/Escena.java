fc07b9f3ce06005abdef4c4fbfcdcd8b0bdf5d9c
Jaime <jai_mi_to@hotmail.es>
2015-11-26 15:52:04 +0100

    Arreglada la mierdosa escena del gato felinoso

--- RayTracer/src/trazador/Escena.java
+++ RayTracer/src/trazador/Escena.java
@@ -18,11 +18,11 @@ public class Escena {
 
 		// esferas
 //		Esfera esfera1 = new Esfera(3, new Material(Color.RED, 0.9, 0.4, 50));
-//		Material mesf2 = new Material(Color.CYAN, 0.9, 0.1, 1);
-//		Esfera esfera2 = new Esfera(new Vector4(20, 20, 20, 1), 5, mesf2);
+		Material mesf2 = new Material(Color.CYAN, 0.4, 0.1, 0.5, 0.5, 70);
+		Esfera esfera2 = new Esfera(new Vector4(30, 30, 30, 1), 15, mesf2);
 		
-//		Material luz = new Material(Color.ORANGE, 0.9, 1, 1);
-//		Esfera sol = new Esfera(new Vector4(40, -40, 40, 0), 5, luz);
+		Material luz = new Material(Color.RED, 0.4, 0.6, 1, 0, 200);
+		Esfera sol = new Esfera(new Vector4(110, 110, 110, 0), 15, luz);
 
 		// PLANOS
 		
@@ -32,7 +32,7 @@ public class Escena {
 		Vector4 p3Plano2 = new Vector4(50, 0, 0, 1);
 		Vector4 p4Plano2 = new Vector4(0, 0, 0, 1);
 		Plano plano2 = new Plano(p1Plano2, p2Plano2,
-				p3Plano2, p4Plano2, new Material(Color.GREEN, 0.3, 0.2, 10));
+				p3Plano2, p4Plano2, new Material(Color.GREEN, 0.2, 0.8, 0, 0, 200));
 		
 		// pared derecha (x = 0)
 		Vector4 p1Plano3 = new Vector4(0, 0, 50, 1);
@@ -40,15 +40,15 @@ public class Escena {
 		Vector4 p3Plano3 = new Vector4(0, 0, 0, 1);
 		Vector4 p4Plano3 = new Vector4(0, 50, 0, 1);
 		Plano plano3 = new Plano(p1Plano3, p2Plano3,
-				p3Plano3, p4Plano3, new Material(Color.BLUE, 0.2, 0.3, 10));
+				p3Plano3, p4Plano3, new Material(Color.BLUE, 0.5, 0.5, 0, 0, 200));
 		
 		// suelo (z = 0)
-//		Vector4 p1Plano1 = new Vector4(0, 0, 0, 1);
-//		Vector4 p2Plano1 = new Vector4(0, 50, 0, 1);
-//		Vector4 p3Plano1 = new Vector4(50, 0, 0, 1);
-//		Vector4 p4Plano1 = new Vector4(50, 50, 0, 1);
-//		Plano plano1 = new Plano(p1Plano1, p2Plano1,
-//				p3Plano1, p4Plano1, new Material(Color.RED, 0.2, 0.3, 10));
+		Vector4 p1Plano1 = new Vector4(0, 0, 0, 1);
+		Vector4 p2Plano1 = new Vector4(0, 50, 0, 1);
+		Vector4 p3Plano1 = new Vector4(50, 0, 0, 1);
+		Vector4 p4Plano1 = new Vector4(50, 50, 0, 1);
+		Plano plano1 = new Plano(p1Plano1, p2Plano1,
+				p3Plano1, p4Plano1, new Material(Color.RED, 0.5, 0.5, 0.1, 0, 200));
 		
 		// triangulos
 //		Vector4 p1Triang1 = new Vector4(0, 0, 0, 1);
@@ -56,22 +56,29 @@ public class Escena {
 //		Vector4 p3Triang1 = new Vector4(30, 30, 0, 1);
 //		Triangulo triangulo1 = new Triangulo(p1Plano1, p2Plano1, p3Plano1, new Material(Color.WHITE, 0.8, 0.2, 1));
 
-//		Esfera esferaT1 = new Esfera(p1Plano1, 5, new Material(Color.RED, 0.2, 0.2, 1));
-//		Esfera esferaT2 = new Esfera(p2Plano1, 5, new Material(Color.GREEN, 0.2, 0.2, 1));
-//		Esfera esferaT3 = new Esfera(p3Plano1, 5, new Material(Color.BLUE, 0.2, 0.2, 1));
-//		Esfera esferaT4 = new Esfera(p4Plano1, 5, new Material(Color.WHITE, 0.2, 0.2, 1));
-		
+		Vector4 origen = new Vector4(0,0,0,1);
+		Vector4 ejex = new Vector4(30,0,0,1);
+		Vector4 ejey = new Vector4(0,50,0,1);
+		Vector4 ejez = new Vector4(0,0,50,1);
+		Esfera esferaT1 = new Esfera(origen, 10, new Material(Color.RED, 0.2, 0.2, 0, 0, 10));
+		Esfera esferaT2 = new Esfera(ejex, 10, new Material(Color.GREEN, 0.2, 0.2, 0, 0, 10));
+		Esfera esferaT3 = new Esfera(ejey, 10, new Material(Color.BLUE, 0.2, 0.2, 0, 0, 10));
+		Esfera esferaT4 = new Esfera(ejez, 10, new Material(Color.WHITE, 0.2, 0.2, 0, 0, 10));
 		
 		// agrega todos los objetos
 //		objetos.add(esfera1);
 //		objetos.add(esfera2);
 //		objetos.add(sol);
 		
-//		objetos.add(plano1);
+		objetos.add(plano1);
 		objetos.add(plano2);
 		objetos.add(plano3);
+		
+		objetos.add(esferaT1);
+		objetos.add(esferaT2);
+		objetos.add(esferaT3);
+		objetos.add(esferaT4);
 		
 		return objetos;
 	}
 }
-
