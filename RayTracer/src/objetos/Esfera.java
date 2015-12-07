ba1020e3c0a7b1f8a0b8a45fac9c1aac91790b7a
Jaime <jai_mi_to@hotmail.es>
2015-12-07 13:28:56 +0100

    Deprecados muchos constructores de Esfera, creados dos

--- RayTracer/src/objetos/Esfera.java
+++ RayTracer/src/objetos/Esfera.java
@@ -4,6 +4,7 @@ import Jama.Matrix;
 import data.Par;
 import data.Rayo;
 import data.Vector4;
+import utils.TransformacionesAfines;
 
 public class Esfera extends Objeto {
 
@@ -19,6 +20,7 @@ public class Esfera extends Objeto {
 	 * @param radio:
 	 *            radio de la esfera
 	 */
+	@Deprecated
 	public Esfera(Vector4 centro, double radio, Material m, Matrix T) {
 		this.centro = centro;
 		this.radio = radio;
@@ -33,6 +35,7 @@ public class Esfera extends Objeto {
 	 * @param radio:
 	 *            radio de la esfera
 	 */
+	@Deprecated
 	public Esfera(double radio, Material m, Matrix T) {
 		this.centro = new Vector4(0, 0, 0, 1);
 		this.radio = radio;
@@ -49,6 +52,7 @@ public class Esfera extends Objeto {
 	 * @param radio:
 	 *            radio de la esfera
 	 */
+	@Deprecated
 	public Esfera(Vector4 centro, double radio, Material m) {
 		this.centro = centro;
 		this.radio = radio;
@@ -62,12 +66,40 @@ public class Esfera extends Objeto {
 	 * @param radio:
 	 *            radio de la esfera
 	 */
+	@Deprecated
 	public Esfera(double radio, Material m) {
 		this.centro = new Vector4(0, 0, 0, 1);
 		this.radio = radio;
 		lowerBound = new Vector4(centro.getX() - radio, centro.getY() - radio, centro.getZ() - radio, 1);
 		upperBound = new Vector4(centro.getX() + radio, centro.getY() + radio, centro.getZ() + radio, 1);
 		super.material = m;
+		updateBounds();
+	}
+
+	/**
+	 * @param material:
+	 *            material de la esfera
+	 */
+	public Esfera(Material m) {
+		this.centro = new Vector4(0, 0, 0, 1);
+		this.radio = 1;
+		super.material = m;
+		updateBounds();
+	}
+
+	/**
+	 * @param radio:
+	 *            radio de la esfera
+	 * 
+	 * @param radio:
+	 *            radio de la esfera
+	 */
+	public Esfera(Material m, Matrix T) {
+		this.centro = new Vector4(0, 0, 0, 1);
+		TransformacionesAfines.multiplyVectorByMatrix(this.centro, T);
+		this.radio = T.get(0, 0);
+		super.material = m;
+		super.T = T;
 		updateBounds();
 	}
 
@@ -81,29 +113,26 @@ public class Esfera extends Objeto {
 	public Par interseccion(Rayo ray) {
 		return interseccionSombra(ray);
 		/*
-		double A = Vector4.dot(ray.getDireccion(), ray.getDireccion()); // A = d
-																		// . d
-		double B = 2 * Vector4.dot(Vector4.sub(ray.getOrigen(), this.centro), ray.getDireccion()); // B
-																								// =
-																								// (a-c).d
-		double C = Vector4.dot(Vector4.sub(ray.getOrigen(), this.centro), Vector4.sub(ray.getOrigen(), this.centro))
-				- Math.pow(this.radio, 2); // C = (a-c).(a-c) - r^2
-
-		double D = Math.pow(B, 2) - 4 * A * C; // D = B^2 - AC
-
-		if (D < 0) {
-			return null;
-		} else {
-			double lambda1, lambda2;
-			// l = [-B +- raiz(B^2-4AC)] / [2A]
-			lambda1 = (-B + Math.pow(Math.pow(B, 2) - 4 * A * C, 0.5)) / (2 * A);
-			lambda2 = (-B - Math.pow(Math.pow(B, 2) - 4 * A * C, 0.5)) / (2 * A);
-
-			double min = Math.min(lambda1, lambda2);
-
-			return new Par(min,this);
-			
-		}*/
+		 * double A = Vector4.dot(ray.getDireccion(), ray.getDireccion()); // A
+		 * = d // . d double B = 2 * Vector4.dot(Vector4.sub(ray.getOrigen(),
+		 * this.centro), ray.getDireccion()); // B // = // (a-c).d double C =
+		 * Vector4.dot(Vector4.sub(ray.getOrigen(), this.centro),
+		 * Vector4.sub(ray.getOrigen(), this.centro)) - Math.pow(this.radio, 2);
+		 * // C = (a-c).(a-c) - r^2
+		 * 
+		 * double D = Math.pow(B, 2) - 4 * A * C; // D = B^2 - AC
+		 * 
+		 * if (D < 0) { return null; } else { double lambda1, lambda2; // l =
+		 * [-B +- raiz(B^2-4AC)] / [2A] lambda1 = (-B + Math.pow(Math.pow(B, 2)
+		 * - 4 * A * C, 0.5)) / (2 * A); lambda2 = (-B - Math.pow(Math.pow(B, 2)
+		 * - 4 * A * C, 0.5)) / (2 * A);
+		 * 
+		 * double min = Math.min(lambda1, lambda2);
+		 * 
+		 * return new Par(min,this);
+		 * 
+		 * }
+		 */
 	}
 
 	/**
@@ -113,13 +142,13 @@ public class Esfera extends Objeto {
 	 *         unica) si intersecta
 	 */
 	@Override
-	public Par interseccionSombra(Rayo ray) {	
-		
+	public Par interseccionSombra(Rayo ray) {
+
 		double A = Vector4.dot(ray.getDireccion(), ray.getDireccion()); // A = d
 																		// . d
 		double B = 2 * Vector4.dot(Vector4.sub(ray.getOrigen(), this.centro), ray.getDireccion()); // B
-																								// =
-																								// (a-c).d
+																									// =
+																									// (a-c).d
 		double C = Vector4.dot(Vector4.sub(ray.getOrigen(), this.centro), Vector4.sub(ray.getOrigen(), this.centro))
 				- Math.pow(this.radio, 2); // C = (a-c).(a-c) - r^2
 
@@ -137,11 +166,11 @@ public class Esfera extends Objeto {
 			double max = Math.max(lambda1, lambda2);
 
 			if (min < 0 && max >= 0)
-				return new Par(max,this);
+				return new Par(max, this);
 			else if (min < 0 && max < 0)
 				return null;
 			else
-				return new Par(min,this);
+				return new Par(min, this);
 		}
 	}
 
@@ -172,9 +201,9 @@ public class Esfera extends Objeto {
 		lowerBound = new Vector4(centro.getX() - radio, centro.getY() - radio, centro.getZ() - radio, 1);
 		upperBound = new Vector4(centro.getX() + radio, centro.getY() + radio, centro.getZ() + radio, 1);
 	}
-	
+
 	@Override
-	public boolean estaDentro(Rayo r, Vector4 interseccion){
+	public boolean estaDentro(Rayo r, Vector4 interseccion) {
 		Vector4 normal = this.normal(interseccion);
 		return Vector4.dot(normal, r.getDireccion()) >= 0;
 	}
